227e304ea1883d0aa3e3426bc3f2bec59b675caf
adrianopb <adrianopbraga1@gmail.com>
2019-10-08 00:07:57 -0300

    Ajustes para utilizar o swagger

--- transacao-cartao-api/transacao-cartao-api/Entities/Pagamento.cs
+++ transacao-cartao-api/transacao-cartao-api/Entities/Pagamento.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace transacao_cartao_api.Entities
+﻿namespace transacao_cartao_api.Entities
 {
     public class Payment
     {
